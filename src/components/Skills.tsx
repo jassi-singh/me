9b1883324e4fae1f6c8d6b892fa5b04447228996
Jassi <jaswinder.dandyan2000@gmail.com>
2023-04-11 23:31:54 +0530

    refactoring in skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Skill, { SkillLevel, SkillProps } from "./Skill";
+import Skill, { SkillProps } from "./Skill";
 import Title from "./Title";
 import { RiFlutterFill } from "react-icons/ri";
 import { FaNode, FaReact } from "react-icons/fa";
@@ -24,92 +24,74 @@ const skills: SkillProps[] = [
   {
     name: "Flutter",
     icon: <RiFlutterFill />,
-    level: SkillLevel.Advanced,
   },
   {
     name: "React",
     icon: <FaReact />,
-    level: SkillLevel.Intermediate,
   },
   {
     name: "Redux",
     icon: <SiRedux />,
-    level: SkillLevel.Intermediate,
   },
   {
     name: "TypeScript",
     icon: <SiTypescript />,
-    level: SkillLevel.Intermediate,
   },
   {
     name: "Dart",
     icon: <SiDart />,
-    level: SkillLevel.Intermediate,
   },
   {
     name: "JavaScript",
     icon: <SiJavascript />,
-    level: SkillLevel.Intermediate,
   },
   {
     name: "Next.js",
     icon: <TbBrandNextjs />,
-    level: SkillLevel.Intermediate,
   },
   {
     name: "Node.js",
     icon: <FaNode />,
-    level: SkillLevel.Basic,
   },
   {
     name: "HTML",
     icon: <SiHtml5 />,
-    level: SkillLevel.Intermediate,
   },
   {
     name: "CSS",
     icon: <SiCss3 />,
-    level: SkillLevel.Intermediate,
   },
   {
     name: "Tailwind CSS",
     icon: <SiTailwindcss />,
-    level: SkillLevel.Advanced,
   },
   {
     name: "MongoDB",
     icon: <DiMongodb />,
-    level: SkillLevel.Intermediate,
   },
   {
     name: "Firebase",
     icon: <SiFirebase />,
-    level: SkillLevel.Advanced,
   },
   {
     name: "Git",
     icon: <DiGit />,
-    level: SkillLevel.Intermediate,
   },
   {
     name: "Appwrite",
     icon: <SiAppwrite />,
-    level: SkillLevel.Intermediate,
   },
   {
     name: "C/C++",
     icon: <TbBrandCpp />,
-    level: SkillLevel.Intermediate,
   },
   {
     name: "Python",
     icon: <DiPython />,
-    level: SkillLevel.Intermediate,
   },
   {
     name: "Solidity",
     icon: <SiSolidity />,
-    level: SkillLevel.Basic,
   },
 ];
 
@@ -123,11 +105,7 @@ const Skills = () => {
   }, [isInViewport]);
 
   return (
-    <section
-      id="skills"
-      ref={ref}
-      className="min-h-screen px-20 sm:px-40 xl:px-60 py-24 sm:overflow-x-hidden"
-    >
+    <section id="skills" ref={ref} className="px-20 sm:px-40 xl:px-60">
       <div
         className={clsx("transition-all ease-in-out duration-1000", {
           "translate-x-full opacity-0": !show,
@@ -137,7 +115,7 @@ const Skills = () => {
       </div>
       <div
         className={clsx(
-          "transition-all ease-in-out duration-1000 grid md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 mt-10 gap-6",
+          "transition-all ease-in-out duration-1000 flex flex-wrap place-content-center mt-10 gap-6",
           {
             "-translate-x-full opacity-0": !show,
           }
