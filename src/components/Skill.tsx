4f831ea6ee4413b866ffe8177b63dc592fd33bf9
Jassi <jaswinder.dandyan2000@gmail.com>
2023-04-13 00:22:25 +0530

    icon size on small devices

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -8,7 +8,7 @@ export interface SkillProps {
 const Skill = (props: SkillProps) => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="text-6xl text-primary">{props.icon}</div>
+      <div className="text-4xl sm:text-6xl text-primary">{props.icon}</div>
       <h1 className="text-lightSlate text-sm font-mono py-3 text-center">
         {props.name}
       </h1>
